24dfb37f14eaa2ce577d64e54a36390e8ed51f81
hiyongliz <644220198@qq.com>
2024-10-21 18:03:20 +0800

    Fix SHELL environment variable not set exception and add installation message

--- install_aliases.py
+++ install_aliases.py
@@ -4,30 +4,33 @@ import os
 def main():
     shell = os.getenv('SHELL')
     if not shell:
-        raise Exception('SHELL environment variable not set')
+        raise Exception('SHELL environment variable not set.')
 
-    rc_file = None
     if shell == '/bin/bash':
         rc_filepath = os.path.expanduser('~/.bashrc')
     elif shell == '/bin/zsh':
         rc_filepath = os.path.expanduser('~/.zshrc')
     else:
-        raise Exception('Unsupported shell: {}'.format(shell))
+        raise Exception('Unsupported shell: {}.'.format(shell))
 
     with open(rc_filepath, 'r', encoding="utf-8") as f:
-        rc_file = f.readlines()
-        for line in rc_file:
-            if line.strip().startswith("#"):
-                continue
-            if "[ -f ~/.aliases ] && source ~/.aliases" in line:
-                print("Already installed")
-                return
+        lines = f.readlines()
+
+    for line in lines:
+        if line.strip().startswith("#"):
+            continue
+        if "[ -f ~/.aliases ] && source ~/.aliases" in line:
+            print("Already installed, Nothing to do.")
+            return
+
+    print("Installing aliases...")
 
     # Move .aliases to ~/.alisase
     os.system('cp .aliases ~/.aliases')
-
     with open(rc_filepath, 'a', encoding="utf-8") as f:
         f.write('\n[ -f ~/.aliases ] && source ~/.aliases\n')
+
+    print("Done!")
 
 
 if __name__ == "__main__":
