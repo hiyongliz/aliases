c8296ec40bc250b4b8816b7bb256f5e9bc1a5598
hiyongliz <644220198@qq.com>
2024-10-21 15:37:45 +0800

    feat: init commit

---
+++ install_alisase.py
@@ -0,0 +1,34 @@
+import os
+
+
+def main():
+    shell = os.getenv('SHELL')
+    if not shell:
+        raise Exception('SHELL environment variable not set')
+
+    rc_file = None
+    if shell == '/bin/bash':
+        rc_filepath = os.path.expanduser('~/.bashrc')
+    elif shell == '/bin/zsh':
+        rc_filepath = os.path.expanduser('~/.zshrc')
+    else:
+        raise Exception('Unsupported shell: {}'.format(shell))
+
+    with open(rc_filepath, 'r', encoding="utf-8") as f:
+        rc_file = f.readlines()
+        for line in rc_file:
+            if line.strip().startswith("#"):
+                continue
+            if "[ -f ~/.alisase ] && source ~/.alisase" in line:
+                print("Already installed")
+                return
+
+    # Move .aliases to ~/.alisase
+    os.system('cp .aliases ~/.alisase')
+
+    with open(rc_filepath, 'a', encoding="utf-8") as f:
+        f.write('\n[ -f ~/.alisase ] && source ~/.alisase\n')
+
+
+if __name__ == "__main__":
+    main()
