0f11577ab12c6a14577b168e080c0a8aad511298
hiyongliz <644220198@qq.com>
2025-11-11 17:33:51 +0800

    refactor: improve type hints and optimize alias file handling in install_aliases.py

--- install_aliases.py
+++ install_aliases.py
@@ -2,6 +2,7 @@ import logging
 import os
 from datetime import datetime
 from pathlib import Path
+from typing import List
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -13,22 +14,21 @@ def copy_alias_file() -> None:
     home_dir: Path = Path.home()
     aliases_path: Path = home_dir / ".aliases"
 
-    aliases_file = []
+    aliases_file: List[Path] = []
     for file in Path(".").iterdir():
         if file.name.startswith(".") and file.name.endswith("aliases"):
             aliases_file.append(file)
     if not aliases_file:
         raise Exception("No alias file found in the current directory.")
 
-    aliases_content = ""
+    aliases_content: List[str] = []
     for file in aliases_file:
         with open(file, "r", encoding="utf-8") as f:
-            aliases_content += f"# {file}\n"
-            aliases_content += f.read()
-            aliases_content += "\n"
+            aliases_content.append(f"# {file}")
+            aliases_content.append(f.read())
 
     with open(aliases_path, "w", encoding="utf-8") as f:
-        f.write(aliases_content)
+        f.write("\n".join(aliases_content))
 
 
 def main() -> None:
@@ -53,7 +53,7 @@ def main() -> None:
     rc_filepath: Path = home_dir / f".{shell.split('/')[-1]}rc"
 
     with open(rc_filepath, "r", encoding="utf-8") as f:
-        lines = f.readlines()
+        lines: List[str] = f.readlines()
 
     for line in lines:
         if line.strip().startswith("#"):
